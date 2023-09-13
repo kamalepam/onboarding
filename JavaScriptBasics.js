2e77cb3d10cbfac237c4a39546b484f0ff1cb832
Kamal Yadav <kamal_yadav@epam.com>
2023-09-13 17:26:52 +0530

    added first commit

---
+++ JavaScriptBasics.js
@@ -0,0 +1,27 @@
+//multiplication table 
+const number = parseInt(prompt('Enter an integer: '));
+for(let i = 1; i <= 10; i++) {
+    const result = i * number;
+    console.log(`${number} * ${i} = ${result}`);
+}
+
+
+//print first n prime numbers
+let count=0
+let i,j 
+for(j=2;j<=100;j++)
+{
+	for( i=1;i<=j;i++)
+	{
+	 
+	  if(j%i==0)
+	  count++
+	  
+	}
+
+	if(count==2){
+	console.log(j)
+	}
+	count=0
+    
+}
